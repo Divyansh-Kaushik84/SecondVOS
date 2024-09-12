54b1d8e22a7b0d00ea1f1f2f016b33a691d7201e
Divyansh-Kaushik84 <84divyanshkaushik@gmail.com>
2024-09-13 04:33:16 +0530

    Initial Commit

---
+++ SecondVOS/SecondVOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondVOSApp.swift
+//  SecondVOS
+//
+//  Created by Divyansh Kaushik on 13/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SecondVOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
